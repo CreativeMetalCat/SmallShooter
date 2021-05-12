00480a2beb6cc8ff2fa27131a8e30f28d8bdfdca
CreativeMetalCat <catguy228@gmail.com>
2021-05-12 16:21:06 +1000

    added weapon demo level and added c++

---
+++ Source/SmallShooter/SmallShooter.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SmallShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SmallShooter, "SmallShooter" );
